6fda3ac234264f717be5f376d76635d8c645173c
lucky-quinn <reubenvinod2002@gmail.com>
2023-08-05 10:17:03 +0530

    Webapp feature

--- billing.js
+++ billing.js
@@ -154,3 +154,58 @@ document.getElementById("Britania_bourbon").addEventListener("click", function()
     amount = (Math.round(amount * 100) / 100).toFixed(2)
     billtotal.value = amount
   });
+
+// service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/images'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Check if the browser supports the "beforeinstallprompt" event
+window.addEventListener('beforeinstallprompt', event => {
+  // Prevent the default behavior
+  event.preventDefault();
+  // Store the event for later use
+  deferredPrompt = event;
+  // Show a custom prompt to the user
+  showAddToHomeScreenPrompt();
+});
+
+// Function to show the "Add to Home Screen" prompt
+function showAddToHomeScreenPrompt() {
+  const addToHomeScreenButton = document.getElementById('addToHomeScreenButton');
+  addToHomeScreenButton.style.display = 'block';
+  addToHomeScreenButton.addEventListener('click', () => {
+    // Show the browser's installation prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      // Clear the deferredPrompt
+      deferredPrompt = null;
+      // Hide the custom prompt
+      addToHomeScreenButton.style.display = 'none';
+    });
+  });
+}
